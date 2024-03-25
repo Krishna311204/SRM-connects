83a8347adf2bcfc55611f52327370e4230798b2d
Krishna311204 <118347547+Krishna311204@users.noreply.github.com>
2024-03-25 22:51:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleSellerClick() {
+    alert('You will be redirected to the item listing page.'); // You can replace this with your desired functionality
+  }
+  
+  function handleBuyerClick() {
+    alert('You will be redirected to the items search section.'); // You can replace this with your desired functionality
+  }
+  
